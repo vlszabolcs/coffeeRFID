801f03fe401216490e887e7fb8d059460942bb40
vlszabolcs <vereslakosszabolcs@gmail.com>
2023-08-07 22:11:00 +0200

    log update

--- include/csv.cpp
+++ include/csv.cpp
@@ -9,7 +9,6 @@ struct Data {
   byte rfid2[4];
 }; 
 
-
 bool readCSVFile(const char* filename, Data* dataArray, int maxSize) {
   File dataFile = SD.open(filename);
   if (!dataFile) {
@@ -17,43 +16,36 @@ bool readCSVFile(const char* filename, Data* dataArray, int maxSize) {
     return false;
   }
 
-  int dataCount = 0; // Megszámoljuk az adatok számát
+  int dataCount = 0; 
 
   while (dataFile.available()) {
-    // Ellenőrizzük, hogy még férünk-e el az adatokat tároló tömbben
+    
     if (dataCount >= maxSize) {
       Serial.println("Tul sok adat, nem fer bele a tombbe!");
       dataFile.close();
       return false;
     }
 
-    // Karaktertömb létrehozása a sor méretéhez
     char lineBuffer[256];
     memset(lineBuffer, 0, sizeof(lineBuffer));
-
-    // Sor beolvasása a karaktertömbbe
+    
     int pos = 0;  
     while (dataFile.available() && pos < sizeof(lineBuffer) - 1) {
       char c = dataFile.read();
       lineBuffer[pos++] = c;
-      if (c == '\n') break; // Sorvége jel (newline) esetén kilépünk
+      if (c == '\n') break; 
     }
 
-    // Az adatok feldolgozása és tárolása a struktúra tömbben
     int id, credit;
     byte rfid0[4], rfid1[4], rfid2[4];
     char name[50];
-
-
-    // Az adatok kiolvasása a sorból és az elválasztó karakterek alapján darabolása
+    
     sscanf(lineBuffer, "%d,%[^,],%d,0x%hhx 0x%hhx 0x%hhx 0x%hhx,0x%hhx 0x%hhx 0x%hhx 0x%hhx,0x%hhx 0x%hhx 0x%hhx 0x%hhx",
          &id, name, &credit,
          &rfid0[0], &rfid0[1], &rfid0[2], &rfid0[3],
          &rfid1[0], &rfid1[1], &rfid1[2], &rfid1[3],
          &rfid2[0], &rfid2[1], &rfid2[2], &rfid2[3]);
-
-
-    // Az adatok tárolása a data tömbben
+      
     dataArray[dataCount].id = id;
     dataArray[dataCount].name = String(name);
     dataArray[dataCount].credit = credit;
@@ -61,8 +53,6 @@ bool readCSVFile(const char* filename, Data* dataArray, int maxSize) {
     memcpy(dataArray[dataCount].rfid1, rfid1, sizeof(rfid1));
     memcpy(dataArray[dataCount].rfid2, rfid2, sizeof(rfid2));
 
-    
-    // Az adatok számlálójának növelése
     dataCount++;
   }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
+/*hardware:
+ESP32 Dev kit C
+NFC module V3
+RTC module
+SD card module 
+Piezzo buzzer
+*/
 
 #include <Wire.h>
 #include <WiFi.h>
 #include <NTPClient.h>
 #include <Adafruit_PN532.h>
+
+#include <HTTPClient.h>
+
 #include <csv.cpp>
 #include <secret.h>
 
@@ -13,7 +23,6 @@ Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-
 const char* ssid = wifiSsid;
 const char* password = wifiPwd;
 const int maxDataCount = 16; //dinamikussá tenni! 
@@ -21,17 +30,74 @@ const int maxDataCount = 16; //dinamikussá tenni!
 int coffeePrice=100;
 
 Data dataArray[maxDataCount];
-
 File logFile;
 
-void printByteArray(byte* array, int size) {
+String printByteArray(byte* array, int size) {
+  String byteStr="";
   for (int i = 0; i < size; i++) {
-    Serial.print(array[i], HEX);
+    byteStr+=String(array[i], HEX);
     if (i < size - 1) {
-      Serial.print(" ");
     }
   }
-  Serial.println();
+  return byteStr;
+}
+
+void succesPurchase(uint8_t* nfcData, String name, int credit) {
+  
+  logFile = SD.open(logPath, FILE_APPEND);
+  if (logFile) {
+    logFile.print(timeClient.getEpochTime());
+    logFile.print(",");
+    logFile.print(name);
+    logFile.print(",");
+    logFile.print(printByteArray(nfcData,4));
+    logFile.print(",");
+    logFile.print(credit);
+    logFile.print(",");
+    logFile.println("success");
+    logFile.close();
+  } else {
+    Serial.println("logFile could not open");
+  }
+}
+
+void noCredit(uint8_t* nfcData, String name, int credit) {
+  
+  logFile = SD.open(logPath, FILE_APPEND);
+  if (logFile) {
+    logFile.print(timeClient.getEpochTime());
+    logFile.print(",");
+    logFile.print(name);
+    logFile.print(",");
+    logFile.print(printByteArray(nfcData,4));
+    logFile.print(",");
+    logFile.print(credit);
+    logFile.print(",");
+    logFile.println("not enough credit");
+    logFile.close();
+  } else {
+    Serial.println("logFile could not open");
+  }
+}
+
+int findPersonAndCheckCredit(Data* dataArray, int dataSize, byte* nfcData, int amount) {
+  for (int i = 0; i < dataSize; i++) {
+    if (memcmp(dataArray[i].rfid0, nfcData, sizeof(dataArray[i].rfid0)) == 0 ||
+        memcmp(dataArray[i].rfid1, nfcData, sizeof(dataArray[i].rfid1)) == 0 ||
+        memcmp(dataArray[i].rfid2, nfcData, sizeof(dataArray[i].rfid2)) == 0) {
+        
+      if (dataArray[i].credit >= amount) { // Credit check and deduction
+        dataArray[i].credit -= amount;
+        succesPurchase(dataArray[i].rfid0,dataArray[i].name, dataArray[i].credit);
+        return i;
+
+      } else {
+        noCredit(dataArray[i].rfid0,dataArray[i].name, dataArray[i].credit);
+        return -1; //has not enough credit
+      }
+    }
+  }
+  return -1;
 }
 
 void setup() {
@@ -56,12 +122,7 @@ void setup() {
     // don't do anything more:
     while (1);
   }
-  if (!SD.begin()) {
-    Serial.println("SD failure!");
-    while (1);
-  }
-
-
+ 
   // reading CSV file and store mem
   if (readCSVFile(dbPath, dataArray, maxDataCount)) {
     Serial.println("csv file has been scanned. ");
@@ -69,9 +130,9 @@ void setup() {
       Serial.print("ID: "); Serial.println(dataArray[i].id);
       Serial.print("Name: "); Serial.println(dataArray[i].name);
       Serial.print("Credit: "); Serial.println(dataArray[i].credit);
-      Serial.print("RFID0: "); printByteArray(dataArray[i].rfid0, 4);
-      Serial.print("RFID1: "); printByteArray(dataArray[i].rfid1, 4);
-      Serial.print("RFID2: "); printByteArray(dataArray[i].rfid2, 4);
+      Serial.print("RFID0: "); Serial.println(printByteArray(dataArray[i].rfid0, 4));
+      Serial.print("RFID1: "); Serial.println(printByteArray(dataArray[i].rfid1, 4));
+      Serial.print("RFID2: "); Serial.println(printByteArray(dataArray[i].rfid2, 4));
       Serial.println("----------------------");
     }
 
@@ -79,48 +140,13 @@ void setup() {
     Serial.println("Error reading the CSV file!");
   }
 
-  logFile = SD.open("coffee_log.txt", FILE_WRITE);
-  if (logFile) {
-    logFile.println("Indítás...");
-    logFile.close();
-  } else {
-    Serial.println("Nem sikerült megnyitni a log fájlt.");
-  }
-}
+  timeClient.update();
+  logFile = SD.open(logPath, FILE_APPEND);
+  logFile.print("system started at: ");
+  logFile.println(timeClient.getEpochTime());
+  logFile.close();
 
-
-void log(String logEntry) {
-  File logFile = SD.open(logPath, FILE_WRITE);
-  if (logFile) {
-    logFile.println(logEntry);
-    logFile.close();
-  } else {
-    Serial.println("Nem sikerült megnyitni a log fájlt.");
-  }
-}
-
-
-int findPersonAndCheckCredit(Data* dataArray, int dataSize, byte* nfcData, int amount) {
-  for (int i = 0; i < dataSize; i++) {
-    if (memcmp(dataArray[i].rfid0, nfcData, sizeof(dataArray[i].rfid0)) == 0 ||
-        memcmp(dataArray[i].rfid1, nfcData, sizeof(dataArray[i].rfid1)) == 0 ||
-        memcmp(dataArray[i].rfid2, nfcData, sizeof(dataArray[i].rfid2)) == 0) {
-        
-      if (dataArray[i].credit >= amount) { // Credit check and deduction
-        dataArray[i].credit -= amount;
-        String logPurchase = String(dataArray[i].name)+","+
-                     String(dataArray[i].rfid0, HEX)+","+
-                     String(dataArray[i].credit) ;
-        Serial.println(logPurchase);
-        log(logPurchase);
-        return i; // Sikeresen levonás, visszaadjuk a személy indexét
-
-      } else {
-        return -1; //has not enough credit
-      }
-    }
-  }
-  return -1; // Az ID nem található az adattömbben
+  postCSV();
 }
 
 void loop(void) {
@@ -131,13 +157,8 @@ void loop(void) {
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength; 
   
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
   
- 
- 
     if (success) {
     Serial.println("RFID card detected!");
 
